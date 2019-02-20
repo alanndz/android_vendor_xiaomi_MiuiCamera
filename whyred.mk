d1b9d199fd2a8567a6b87e4eb37321131bda25ed
mdeejay <mdjrussia@gmail.com>
2019-02-20 08:05:04 +0100

    Adapt to BeastROMs

--- whyred.mk
+++ whyred.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
+$(call inherit-product, vendor/MiuiCamera/config.mk)
 
-VENDOR_PATH := vendor/xiaomi/MiuiCamera
+VENDOR_PATH := vendor/MiuiCamera
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/system/etc/device_features/whyred.xml:system/etc/device_features/whyred.xml
